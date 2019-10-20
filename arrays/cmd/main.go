1939a0f1bdccacd453435687943bd695072bfd6d
Steve de Silva <steve.desilva@gmail.com>
2019-10-20 21:11:28 +0100

    getAndSetArrayElements

---
+++ arrays/cmd/main.go
@@ -0,0 +1,278 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	// emptyArray()
+	getAndSetArrayElements()
+}
+
+// ---------------------------------------------------------
+// EXERCISE: Declare empty arrays
+//
+//  1. Declare and print the following arrays with their types:
+//
+//    1. The names of your best three friends (names array)
+//
+//    2. The distances to five different locations (distances array)
+//
+//    3. A data buffer with five bytes of capacity (data array)
+//
+//    4. Currency exchange ratios only for a single currency (ratios array)
+//
+//    5. Up/Down status of four different web servers (alives array)
+//
+//    6. A byte array that doesn't occupy memory space (zero array)
+//
+//  2. Print only the types of the same arrays.
+//
+//  3. Print only the elements of the same arrays.
+//
+// HINT
+//   When printing the elements of an array, you can use the usual Printf verbs.
+//
+//   For example:
+//     When printing a string array, you can use "%q" verb as usual.
+//
+// EXPECTED OUTPUT
+//  names    : [3]string{"", "", "", ""}
+//  distances: [5]int{0, 0, 0, 0, 0}
+//  data     : [5]uint8{0x0, 0x0, 0x0, 0x0, 0x0}
+//  ratios   : [1]float64{0}
+//  alives   : [4]bool{false, false, false, false}
+//  zero     : [0]uint8{}
+//
+//  names    : [3]string
+//  distances: [5]int
+//  data     : [5]uint8
+//  ratios   : [1]float64
+//  alives   : [4]bool
+//  zero     : [0]uint8
+//
+//  names    : ["" "" ""]
+//  distances: [0 0 0 0 0]
+//  data     : [0 0 0 0 0]
+//  ratios   : [0.00]
+//  alives   : [false false false false]
+//  zero     : []
+// ---------------------------------------------------------
+func emptyArray() {
+	var (
+		names     [3]string
+		distances [5]int
+		data      [5]uint8
+		ratios    [1]float64
+		alives    [4]bool
+		zero      [0]uint8
+	)
+	// 1. Declare and print the arrays with their types.
+	fmt.Printf("names    : %#v\n", names)
+	fmt.Printf("distances: %#v\n", distances)
+	fmt.Printf("data     : %#v\n", data)
+	fmt.Printf("ratios   : %#v\n", ratios)
+	fmt.Printf("alives   : %#v\n", alives)
+	fmt.Printf("zero     : %#v\n", zero)
+
+	//  2. Print only the types of the same arrays.
+	fmt.Println()
+	fmt.Printf("names    : %T\n", names)
+	fmt.Printf("distances: %T\n", distances)
+	fmt.Printf("data     : %T\n", data)
+	fmt.Printf("ratios   : %T\n", ratios)
+	fmt.Printf("alives   : %T\n", alives)
+	fmt.Printf("zero     : %T\n", zero)
+
+	//  3. Print only the elements of the same arrays.
+	fmt.Println()
+	fmt.Printf("names    : %q\n", names)
+	fmt.Printf("distances: %d\n", distances)
+	fmt.Printf("data     : %d\n", data)
+	fmt.Printf("ratios   : %.2f\n", ratios)
+	fmt.Printf("alives   : %t\n", alives)
+	fmt.Printf("zero     : %d\n", zero)
+
+}
+
+// ---------------------------------------------------------
+// EXERCISE: Get and Set Array Elements
+//
+//  1. Use the 01-declare-empty exercise
+//  2. Remove everything but the array declarations
+//
+//  3. Assign your best friends' names to the names array
+//
+//  4. Assign distances to the closest cities to you to the distance array
+//
+//  5. Assign arbitrary bytes to the data array
+//
+//  6. Assign a value to the ratios array
+//
+//  7. Assign true/false values to the alives arrays
+//
+//  8. Try to assign to the zero array and observe the error
+//
+//  9. Now use ordinary loop statements for each array and print them
+//      (do not use for range)
+//
+//  10. Now use for range loop statements for each array and print them
+//
+//  11. Try assigning different types of values to the arrays, break things,
+//     and observe the errors
+//
+//  12. Remove some of the array assignments and observe the loop outputs
+//      (zero values)
+//
+//
+// EXPECTED OUTPUT
+//
+//  Note: The output can change depending on the values that you've assigned to them, of course.
+//        You're free to assign any values.
+//
+//   names
+//   ====================
+//   names[0]: "Einstein"
+//   names[1]: "Tesla"
+//   names[2]: "Shepard"
+//
+//   distances
+//   ====================
+//   distances[0]: 50
+//   distances[1]: 40
+//   distances[2]: 75
+//   distances[3]: 30
+//   distances[4]: 125
+//
+//   data
+//   ====================
+//   data[0]: 72
+//   data[1]: 69
+//   data[2]: 76
+//   data[3]: 76
+//   data[4]: 79
+//
+//   ratios
+//   ====================
+//   ratios[0]: 3.14
+//
+//   alives
+//   ====================
+//   alives[0]: true
+//   alives[1]: false
+//   alives[2]: true
+//   alives[3]: false
+//
+//   zero
+//   ====================
+
+//
+//   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//   FOR RANGES
+//   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//   names
+//   ====================
+//   names[0]: "Einstein"
+//   names[1]: "Tesla"
+//   names[2]: "Shepard"
+//
+//   distances
+//   ====================
+//   distances[0]: 50
+//   distances[1]: 40
+//   distances[2]: 75
+//   distances[3]: 30
+//   distances[4]: 125
+//
+//   data
+//   ====================
+//   data[0]: 72
+//   data[1]: 69
+//   data[2]: 76
+//   data[3]: 76
+//   data[4]: 79
+//
+//   ratios
+//   ====================
+//   ratios[0]: 3.14
+//
+//   alives
+//   ====================
+//   alives[0]: true
+//   alives[1]: false
+//   alives[2]: true
+//   alives[3]: false
+//
+//   zero
+//   ====================
+//
+// ---------------------------------------------------------
+
+func getAndSetArrayElements() {
+	var (
+		names     [3]string
+		distances [5]int
+		data      [5]uint8
+		ratios    [1]float64
+		alives    [4]bool
+		zero      [0]uint8
+	)
+
+	names[0], names[1], names[2] = "Tyson", "Ali", "Frazier"
+
+	distances[0] = 50
+	distances[1] = 40
+	distances[2] = 75
+	distances[3] = 30
+	distances[4] = 125
+
+	data[0] = 'H'
+	data[1] = 'E'
+	data[2] = 'L'
+	data[3] = 'L'
+	data[4] = 'O'
+	
+	ratios[0] = 3.14145
+
+	alives[0] = true
+	alives[1] = false
+	alives[2] = true
+	alives[3] = false
+
+	separator := "\n" + strings.Repeat("=", 20) + "\n"
+
+	fmt.Print("names", separator)
+	for i := 0; i < len(names); i++ {
+		fmt.Printf("names[%d]: %q\n", i, names[i])
+	}
+
+	fmt.Print("\ndistances", separator)
+	for i := 0; i < len(distances); i++ {
+		fmt.Printf("distances[%d]: %d\n", i, distances[i])
+	}
+
+	fmt.Print("\ndata", separator)
+	for i := 0; i < len(data); i++ {
+		// try the %c verb
+		fmt.Printf("data[%d]: %c\n", i, data[i])
+	}
+
+	fmt.Print("\nratios", separator)
+	for i := 0; i < len(ratios); i++ {
+		fmt.Printf("ratios[%d]: %.2f\n", i, ratios[i])
+	}
+
+	fmt.Print("\nalives", separator)
+	for i := 0; i < len(alives); i++ {
+		fmt.Printf("alives[%d]: %t\n", i, alives[i])
+	}
+
+	// no loop for zero elements
+	fmt.Print("\nzero", separator)
+	for i := 0; i < len(zero); i++ {
+		fmt.Printf("zero[%d]: %d\n", i, zero[i])
+	}
+
+}
