4ae6a8df760e188b654861c8764c82a99363e110
Steve de Silva <steve.desilva@gmail.com>
2019-10-20 22:03:57 +0100

    arrayLiterals

--- arrays/cmd/main.go
+++ arrays/cmd/main.go
@@ -7,7 +7,8 @@ import (
 
 func main() {
 	// emptyArray()
-	getAndSetArrayElements()
+	// getAndSetArrayElements()
+	arrayLiterals()
 }
 
 // ---------------------------------------------------------
@@ -312,4 +313,72 @@ FOR RANGES
 	for i, v := range zero {
 		fmt.Printf("zero[%d]: %d\n", i, v)
 	}
+
+}
+
+// ---------------------------------------------------------
+// EXERCISE: Refactor to Array Literals
+//
+//  1. Use the 02-get-set-arrays exercise
+//
+//  2. Refactor the array assignments to array literals
+//
+//    1. You would need to change the array declarations to array literals
+//
+//    2. Then, you would need to move the right-hand side of the assignments,
+//       into the array literals.
+//
+// EXPECTED OUTPUT
+//   The output should be the same as the 02-get-set-arrays exercise.
+// ---------------------------------------------------------
+
+func arrayLiterals() {
+
+	names := [3]string{"Einstein", "Tesla", "Shepard"}
+	distances := [5]int{50, 40, 75, 30, 125}
+	data := [5]uint8{'H', 'E', 'L', 'L', 'O'}
+	ratios := [1]float64{3.14145}
+	alives := [4]bool{true, false, true, false}
+	var zero [0]uint8
+
+	separator := "\n" + strings.Repeat("=", 20) + "\n"
+
+	fmt.Printf(`
+%s
+FOR RANGES
+%[1]s
+`, strings.Repeat("~", 30))
+
+	fmt.Print("names", separator)
+	for i, v := range names {
+		fmt.Printf("names[%d]: %q\n", i, v)
+	}
+
+	fmt.Print("\ndistances", separator)
+	for i, v := range distances {
+		fmt.Printf("distances[%d]: %d\n", i, v)
+	}
+
+	fmt.Print("\ndata", separator)
+	for i, v := range data {
+		// try the %c verb
+		fmt.Printf("data[%d]: %d\n", i, v)
+	}
+
+	fmt.Print("\nratios", separator)
+	for i, v := range ratios {
+		fmt.Printf("ratios[%d]: %.2f\n", i, v)
+	}
+
+	fmt.Print("\nalives", separator)
+	for i, v := range alives {
+		fmt.Printf("alives[%d]: %t\n", i, v)
+	}
+
+	// no loop for zero elements
+	fmt.Print("\nzero", separator)
+	for i, v := range zero {
+		fmt.Printf("zero[%d]: %d\n", i, v)
+	}
+
 }
