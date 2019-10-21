52e20e204db8e71d765d9c14e33df2efd3ed08b0
Steve de Silva <steve.desilva@gmail.com>
2019-10-21 07:37:07 +0100

    comparableArrays task start

--- arrays/cmd/main.go
+++ arrays/cmd/main.go
@@ -9,7 +9,8 @@ func main() {
 	// emptyArray()
 	// getAndSetArrayElements()
 	// arrayLiterals()
-	simplify()
+	// simplify()
+	comparableArrays()
 }
 
 // ---------------------------------------------------------
@@ -399,4 +400,35 @@ func simplify() {
 
 	fmt.Printf("%q\n", names)
 	fmt.Printf("%q\n", books)
+}
+
+// ---------------------------------------------------------
+// EXERCISE: Compare the Arrays
+//
+//  1. Uncomment the code
+//
+//  2. Fix the problems so that arrays become comparable
+//
+// EXPECTED OUTPUT
+//  true
+//  true
+//  false
+// ---------------------------------------------------------
+func comparableArrays() {
+	// week := [...]string{"Monday", "Tuesday"}
+	// wend := [4]string{"Saturday", "Sunday"}
+
+	// fmt.Println(week != wend)
+
+	// evens := [...]int{2, 4, 6, 8, 10}
+	// evens2 := [...]int32{2, 4, 6, 8, 10}
+
+	// fmt.Println(evens == evens2)
+
+	// Use     : uint8 for one of the arrays instead of byte here.
+	// Remember: Aliased types are the same types.
+	image := [5]byte{'h', 'i'}
+	var data [5]byte
+
+	fmt.Println(data == image)
 }
