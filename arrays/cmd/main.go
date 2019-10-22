4e36808af410cafc3ee31e7d58df4b1aff24a3ca
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2019-10-23 00:16:18 +0100

    wizardPrinter

--- arrays/cmd/main.go
+++ arrays/cmd/main.go
@@ -16,7 +16,8 @@ func main() {
 	// comparableArrays()
 	// assignmentOfArrays()
 	// moodlySingleArray()
-	moodlyMultiArray()
+	// moodlyMultiArray()
+	wizardPrinter()
 }
 
 // ---------------------------------------------------------
@@ -609,4 +610,39 @@ func moodlyMultiArray() {
 	}
 
 	fmt.Printf("%s feels %s\n", name, moods[moodVal][n])
+}
+
+// ---------------------------------------------------------
+// EXERCISE: Wizard Printer
+//
+//   In this exercise, your goal is to display a few famous scientists
+//   in a pretty table.
+//
+//   1. Create a multi-dimensional array
+//   2. In each inner array, store the scientist's name, lastname and his/her
+//      nickname
+//   3. Print their information in a pretty table using a loop.
+//
+// EXPECTED OUTPUT
+//   First Name      Last Name       Nickname
+//   ==================================================
+//   Albert          Einstein        time
+//   Isaac           Newton          apple
+//   Stephen         Hawking         blackhole
+//   Marie           Curie           radium
+//   Charles         Darwin          fittest
+// ---------------------------------------------------------
+
+func wizardPrinter() {
+	table := [...][3]string{
+		{"Albert", "Einstein", "time"},
+		{"Stephen", "Hawking", "blackhole"},
+		{"Marie", "Curie", "radium"},
+		{"Charles", "Darwin", "fittest"},
+	}
+	fmt.Printf("%-15s %-15s %-15s\n","First Name","Last Name","Nickname")
+	fmt.Println(strings.Repeat("=",50))
+	for _,v:= range table {
+		fmt.Printf("%-15s %-15s %-15s\n",v[0],v[1],v[2])
+	}
 }
