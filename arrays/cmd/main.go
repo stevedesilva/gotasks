7e7bda815082b8166af4dedc6bc4156fd019ed3a
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2019-10-27 11:27:48 +0000

    bubblesort

--- arrays/cmd/main.go
+++ arrays/cmd/main.go
@@ -21,7 +21,8 @@ func main() {
 	// wizardPrinter()
 	// currencyConverter()
 	// searchEngine()
-	average()
+	// average()
+	numberSorter()
 }
 
 // ---------------------------------------------------------
@@ -853,3 +854,89 @@ func average() {
 	fmt.Printf("Average: %v \n", sum/float64(count))
 
 }
+
+// ---------------------------------------------------------
+// EXERCISE: Number Sorter
+//
+//  Your goal is to sort the given numbers from the command-line.
+//
+//  1. Get the numbers from the command-line.
+//
+//  2. Create an array and assign the given numbers to that array.
+//
+//  3. Sort the given numbers and print them.
+//
+// RESTRICTION
+//   + Maximum 5 numbers can be provided
+//   + If one of the arguments are not a valid number, skip it
+//
+// HINTS
+//  + You can use the bubble-sort algorithm to sort the numbers.
+//    Please watch this: https://youtu.be/nmhjrI-aW5o?t=7
+//
+//  + When swapping the elements, do not check for the last element.
+//
+//    Or, you will receive this error:
+//    "panic: runtime error: index out of range"
+//
+// EXPECTED OUTPUT
+//   go run main.go
+//     Please give me up to 5 numbers.
+//
+//   go run main.go 6 5 4 3 2 1
+//     Sorry. Go arrays are fixed. So, for now, I'm only supporting sorting 5 numbers...
+//
+//   go run main.go 5 4 3 2 1
+//     [1 2 3 4 5]
+//
+//   go run main.go 5 4 a c 1
+//     [0 0 1 4 5]
+// ---------------------------------------------------------
+
+func numberSorter() {
+	args := os.Args[1:]
+
+	if l := len(args); l == 0 {
+		fmt.Println("Please give me up to 5 numbers.")
+		return
+	} else if l > 5 {
+		fmt.Println("Sorry. Go arrays are fixed. So, for now, I'm only supporting sorting 5 numbers...")
+		return
+	}
+
+	var (
+		nums [5]float64
+	)
+
+	for i, v := range args {
+		n, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			continue
+		}
+		nums[i] = n
+	}
+
+	fmt.Printf("Received numbers: %v\n", nums)
+	sorted := bubblesort(nums)
+	fmt.Printf("Your numbers: %v\n", sorted)
+
+}
+
+func bubblesort(nums [5]float64) [5]float64 {
+	/*
+		check whether it's the last element or not:
+		  i < len(nums)-1
+		check whether the next number is greater than the current one, if so, swap it:
+		  v > nums[i+1]
+	*/
+	for range nums {
+		for i, v := range nums {
+			if i < len(nums)-1 && v > nums[i+1] {
+				nums[i], nums[i+1] = nums[i+1], nums[i]
+			}
+		}
+	}
+	return nums
+
+}
+
