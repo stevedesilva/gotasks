aa5f9d22c271628cc95ad9f9a950898cf3de32a3
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2019-10-27 09:25:24 +0000

    average func

--- arrays/cmd/main.go
+++ arrays/cmd/main.go
@@ -20,7 +20,8 @@ func main() {
 	// moodlyMultiArray()
 	// wizardPrinter()
 	// currencyConverter()
-	searchEngine()
+	// searchEngine()
+	average()
 }
 
 // ---------------------------------------------------------
@@ -790,5 +791,65 @@ func searchEngine() {
 	if !flag {
 		fmt.Printf("We don't have the book: %q\n", word)
 	}
+
+}
+
+// ---------------------------------------------------------
+// EXERCISE: Find the Average
+//
+//  Your goal is to fill an array with numbers and find the average.
+//
+//  1. Get the numbers from the command-line.
+//
+//  2. Create an array and assign the given numbers to that array.
+//
+//  3. Print the given numbers and their average.
+//
+// RESTRICTION
+//   + Maximum 5 numbers can be provided
+//   + If one of the arguments are not a valid number, skip it
+//
+// EXPECTED OUTPUT
+//   go run main.go
+//     Please tell me numbers (maximum 5 numbers).
+//
+//   go run main.go 1 2 3 4 5 6
+//     Please tell me numbers (maximum 5 numbers).
+//
+//   go run main.go 1 2 3 4 5
+//     Your numbers: [1 2 3 4 5]
+//     Average: 3
+//
+//   go run main.go 1 a 2 b 3
+//     Your numbers: [1 0 2 0 3]
+//     Average: 2
+// ---------------------------------------------------------
+
+func average() {
+	args := os.Args[1:]
+
+	if len := len(args); len < 1 || len > 5 {
+		fmt.Println("Please tell me numbers (maximum 5 numbers).")
+		return
+	}
+
+	var (
+		nums  [5]float64
+		sum   float64
+		count int
+	)
+
+	for i, v := range args {
+		n, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			continue
+		}
+		count++
+		nums[i] = n
+		sum += n
+	}
+
+	fmt.Printf("Your numbers: %v\n", nums)
+	fmt.Printf("Average: %v \n", sum/float64(count))
 
 }
