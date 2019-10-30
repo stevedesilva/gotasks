d4310e817367f3c9edf592d2a861c29ee2b2bf4b
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2019-10-30 08:01:44 +0000

    complete digi clock

--- arrays/digitalclock/digitalClock.go
+++ arrays/digitalclock/digitalClock.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/inancgumus/screen"
 )
 
 func main() {
@@ -104,34 +106,38 @@ func digitalClock() {
 	var digits = [...]placeholder{
 		zero, one, two, three, four, five, six, seven, eight, nine,
 	}
+	screen.Clear()
+	for {
 
-	now := time.Now()
-	hour, min, sec := now.Hour(), now.Minute(), now.Second()
+		screen.MoveTopLeft()
+		now := time.Now()
+		hour, min, sec := now.Hour(), now.Minute(), now.Second()
 
-	fmt.Printf("hour %d, min %d, sec %d\n", hour, min, sec)
-	clock := [...]placeholder{
-		digits[hour/10],digits[hour%10],
-		colon,
-		digits[min/10],digits[min%10],
-		colon,
-		digits[sec/10],digits[sec%10],
-	}
+		// fmt.Printf("hour %d, min %d, sec %d\n", hour, min, sec)
+		clock := [...]placeholder{
+			digits[hour/10], digits[hour%10],
+			colon,
+			digits[min/10], digits[min%10],
+			colon,
+			digits[sec/10], digits[sec%10],
+		}
+		// 0 to 4
+		for line := range clock[0] {
+			// get each digit
+			for index, digit := range clock {
+				// print [0][0], [1][0], [2][0]...
+				// colon blink
+				next := clock[index][line]
+				if digit == colon && sec%2 == 0 {
+					next = "   "
+				}
+				fmt.Print(next, "  ")
 
-	// for _, digit := range digits {
-	// 	for _, line := range digit {
-	// 		fmt.Println(line)
-	// 	}
-	// 	fmt.Println()
-	// }
-
-	// 0 to 4
-	for line := range clock[0] {
-		// get each digit
-		for digit := range clock {
-			// print [0][0], [1][0], [2][0]...
-			fmt.Print(clock[digit][line], " ")
+			}
+			fmt.Println()
 		}
 		fmt.Println()
+		time.Sleep(time.Second)
 	}
 
 }
