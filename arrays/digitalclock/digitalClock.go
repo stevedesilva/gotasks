c9913d9e0adb5fe017118bb6e427e8c6e4dd98f1
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2019-10-30 02:04:43 +0000

    digi clock interim soltution

--- arrays/digitalclock/digitalClock.go
+++ arrays/digitalclock/digitalClock.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
 	digitalClock()
 }
@@ -87,4 +92,46 @@ func digitalClock() {
 		"  █",
 		"███",
 	}
+
+	colon := placeholder{
+		"   ",
+		" ░ ",
+		"   ",
+		" ░ ",
+		"   ",
+	}
+
+	var digits = [...]placeholder{
+		zero, one, two, three, four, five, six, seven, eight, nine,
+	}
+
+	now := time.Now()
+	hour, min, sec := now.Hour(), now.Minute(), now.Second()
+
+	fmt.Printf("hour %d, min %d, sec %d\n", hour, min, sec)
+	clock := [...]placeholder{
+		digits[hour/10],digits[hour%10],
+		colon,
+		digits[min/10],digits[min%10],
+		colon,
+		digits[sec/10],digits[sec%10],
+	}
+
+	// for _, digit := range digits {
+	// 	for _, line := range digit {
+	// 		fmt.Println(line)
+	// 	}
+	// 	fmt.Println()
+	// }
+
+	// 0 to 4
+	for line := range clock[0] {
+		// get each digit
+		for digit := range clock {
+			// print [0][0], [1][0], [2][0]...
+			fmt.Print(clock[digit][line], " ")
+		}
+		fmt.Println()
+	}
+
 }
