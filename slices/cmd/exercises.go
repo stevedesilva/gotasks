23474c559e99f43a48dc65e408d8d668393787d5
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2019-11-02 20:10:08 +0000

    addition

--- slices/cmd/exercises.go
+++ slices/cmd/exercises.go
@@ -10,41 +10,15 @@ package main
 
 import "fmt"
 
-// ---------------------------------------------------------
-// EXERCISE: Declare nil slices
-//
-//  1. Declare the following slices as nil slices:
-//
-//    1. The names of your friends (names slice)
-//
-//    2. The distances to locations (distances slice)
-//
-//    3. A data buffer (data slice)
-//
-//    4. Currency exchange ratios (ratios slice)
-//
-//    5. Up/Down status of web servers (alives slice)
-//
-//
-//  2. Print their type, length and whether they're equal to nil value or not.
-//
-//
-// EXPECTED OUTPUT
-//  names    : []string 0 true
-//  distances: []int 0 true
-//  data     : []uint8 0 true
-//  ratios   : []float64 0 true
-//  alives   : []bool 0 true
-// ---------------------------------------------------------
 
 func main() {
 
 	var (
-		names      []string
-		distances []int
-		data      []uint8
-		ratios    []float64
-		alives    []bool
+		names     []string = []string{}
+		distances []int = []int{}
+		data    []uint8 = []uint8{}
+		ratios    []float64 = []float64{}
+		alives    []bool = []bool{}
 	)
 
 	fmt.Printf("names    : %T %d %t\n", names, len(names), names == nil)
