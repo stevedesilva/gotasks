6d4be3d6cbb194d0649c00151c662a7711bd38c5
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2019-11-02 20:28:08 +0000

    exe 3

--- slices/cmd/exercises.go
+++ slices/cmd/exercises.go
@@ -10,15 +10,44 @@ package main
 
 import "fmt"
 
-
+// ---------------------------------------------------------
+// EXERCISE: Assign slice literals
+//
+//   1. Assign the following data using slice literals to the slices that
+//      you've declared in the first exercise.
+//
+//    1. The names of your best three friends (to the names slice)
+//
+//    2. The distances to five different locations (to the distances slice)
+//
+//    3. Five bytes of data (to the data slice)
+//
+//    4. Two currency exchange ratios (to the ratios slice)
+//
+//    5. Up/Down status of four different web servers (to the alives slice)
+//
+//  2. Print their type, length and whether they're equal to nil value or not
+//
+//  3. Compare the length of the distances and the data slices; print a message
+//     if they are equal (use an if statement).
+//
+//
+// EXPECTED OUTPUT
+//  names    : []string 3 false
+//  distances: []int 5 false
+//  data     : []uint8 5 false
+//  ratios   : []float64 2 false
+//  alives   : []bool 4 false
+//  The length of the distances and the data slices are the same.
+// ---------------------------------------------------------
 func main() {
 
 	var (
-		names     []string = []string{}
-		distances []int = []int{}
-		data    []uint8 = []uint8{}
-		ratios    []float64 = []float64{}
-		alives    []bool = []bool{}
+		names     []string  = []string{"Max", "Dave", "Paul"}
+		distances []int     = []int{3, 4, 5, 6, 7}
+		data      []uint8   = []uint8{'A', 'B', 'N', 128, 5}
+		ratios    []float64 = []float64{2.4, 4.5}
+		alives    []bool    = []bool{true, true, false, false}
 	)
 
 	fmt.Printf("names    : %T %d %t\n", names, len(names), names == nil)
@@ -26,6 +55,5 @@ func main() {
 	fmt.Printf("data     : %T %d %t\n", data, len(data), data == nil)
 	fmt.Printf("ratios   : %T %d %t\n", ratios, len(ratios), ratios == nil)
 	fmt.Printf("alives   : %T %d %t\n", alives, len(alives), alives == nil)
-
 
 }
