b83a06b03454b779848a3237384873f461a0738b
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2019-11-04 23:44:56 +0000

    exercise 3

--- slices/cmd/exercises.go
+++ slices/cmd/exercises.go
@@ -8,7 +8,17 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+func main() {
+	// e1()
+	// e2()
+	e3()
+}
 
 // ---------------------------------------------------------
 // EXERCISE: Assign slice literals
@@ -40,7 +50,7 @@ import "fmt"
 //  alives   : []bool 4 false
 //  The length of the distances and the data slices are the same.
 // ---------------------------------------------------------
-func main() {
+func e1() {
 
 	var (
 		names     []string  = []string{"Max", "Dave", "Paul"}
@@ -56,4 +66,77 @@ func main() {
 	fmt.Printf("ratios   : %T %d %t\n", ratios, len(ratios), ratios == nil)
 	fmt.Printf("alives   : %T %d %t\n", alives, len(alives), alives == nil)
 
+}
+
+// ---------------------------------------------------------
+// EXERCISE: Declare the arrays as slices
+//
+//   1. First run the following program as it is
+//
+//   2. Then change the array declarations to slice declarations
+//
+//   3. Observe whether anything changes or not (on the surface :)).
+//
+// EXPECTED OUTPUT
+//  names    : []string ["Einstein" "Tesla" "Shepard"]
+//  distances: []int [50 40 75 30 125]
+//  data     : []uint8 [72 69 76 76 79]
+//  ratios   : []float64 [3.14]
+//  alives   : []bool [true false true false]
+//  zero     : []uint8 []
+// ---------------------------------------------------------
+// pwd
+func e2() {
+	names := []string{"Einstein", "Tesla", "Shepard"}
+	distances := []int{50, 40, 75, 30, 125}
+	data := []byte{'H', 'E', 'L', 'L', 'O'}
+	ratios := []float64{3.14145}
+	alives := []bool{true, false, true, false}
+	zero := []byte{}
+
+	fmt.Printf("names    : %[1]T %[1]q\n", names)
+	fmt.Printf("distances: %[1]T %[1]d\n", distances)
+	fmt.Printf("data     : %[1]T %[1]d\n", data)
+	fmt.Printf("ratios   : %[1]T %.2[1]f\n", ratios)
+	fmt.Printf("alives   : %[1]T %[1]t\n", alives)
+	fmt.Printf("zero     : %[1]T %[1]d\n", zero)
+}
+
+// ---------------------------------------------------------
+// EXERCISE: Fix the problems
+//
+//  1. Uncomment the code
+//
+//  2. Fix the problems
+//
+//  3. BONUS: Simplify the code
+//
+//
+// EXPECTED OUTPUT
+//   "Einstein and Shepard and Tesla"
+//   ["Fire" "Kafka's Revenge" "Stay Golden"]
+//   [1 2 3 5 6 7 8 9]
+// ---------------------------------------------------------
+
+func e3() {
+
+	names := []string{"Einstein", "Shepard", "Tesla"}
+
+	books := []string{"Stay Golden", "Fire", "Kafka's Revenge"}
+	sort.Strings(books)
+
+	// -----------------------------------
+	// this time, do not change the nums array to a slice
+	nums := [...]int{5, 1, 7, 3, 8, 2, 6, 9}
+
+	// use the slicing expression to change the nums array to a slice below
+	sort.Ints(nums[:])
+
+	// -----------------------------------
+	// Here: Use the strings.Join function to join the names
+	//       (see the expected output)
+	fmt.Printf("%q\n", strings.Join(names, " and "))
+
+	fmt.Printf("%q\n", books)
+	fmt.Printf("%d\n", nums)
 }
