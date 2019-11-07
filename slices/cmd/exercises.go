5ebf051240353747db1b2e99d4c899a3ea41407f
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2019-11-07 00:32:08 +0000

    init commit

--- slices/cmd/exercises.go
+++ slices/cmd/exercises.go
@@ -25,7 +25,8 @@ func main() {
 	// e1Append()
 	// e2Append()
 	// e3Append()
-	e3Sort()
+	// e3Sort()
+	e4HousePrices()
 }
 
 // ---------------------------------------------------------
@@ -307,4 +308,144 @@ func e3Sort() {
 
 	sort.Ints(in)
 	fmt.Printf("%d", in)
+}
+
+// ---------------------------------------------------------
+// EXERCISE: Housing Prices
+//
+//  We have received housing prices. Your task is to load the data into
+//  appropriately typed slices then print them.
+//
+//  1. Check out the expected output
+//
+//
+//  2. Check out the code below
+//
+//     1. header   : stores the column headers
+//     2. data     : stores the real data related to each column
+//     3. separator: you will use it to separate the data
+//
+//
+//  3. Parse the data
+//
+//     1. Separate it into rows by using the newline character ("\n")
+//
+//     2. For each row, separate it by using the separator (",")
+//
+//
+//  4. Load the data into distinct slices
+//
+//     1. Load the locations into a []string
+//     2. Load the sizes into []int
+//     3. Load the beds into []int
+//     4. Load the baths into []int
+//     5. Load the prices into []int
+//
+//
+//  5. Print the header
+//
+//     1. Separate it by using the separator
+//
+//     2. Print each column 15 character wide ("%-15s")
+//
+//
+//  6. Print the rows from the slices that you've created, line by line
+//
+//
+// EXPECTED OUTPUT
+//
+//  Location       Size           Beds           Baths          Price
+//  ===========================================================================
+//  New York       100            2              1              100000
+//  New York       150            3              2              200000
+//  Paris          200            4              3              400000
+//  Istanbul       500            10             5              1000000
+//
+//
+// HINTS
+//
+//  + strings.Split function can separate a string into a []string
+//
+// ---------------------------------------------------------
+
+func e4HousePrices() {
+	const (
+		header = "Location,Size,Beds,Baths,Price"
+		data   = `New York,100,2,1,100000
+New York,150,3,2,200000
+Paris,200,4,3,400000
+Istanbul,500,10,5,1000000`
+
+		separator = ","
+	)
+
+	var (
+		location []string
+		size     []int
+		beds     []int
+		baths    []int
+		prices   []int
+	)
+	row := strings.Split(data, "\n")
+	for _, r := range row {
+		t := strings.Split(r, separator)
+		for idx, info := range t {
+
+			switch idx {
+			case 0:
+				location = append(location, info)
+			case 1:
+				{
+					i, err := strconv.Atoi(info)
+					if err != nil {
+						return
+					}
+					size = append(size, i)
+				}
+			case 2:
+				{
+					i, err := strconv.Atoi(info)
+					if err != nil {
+						return
+					}
+					beds = append(beds, i)
+				}
+
+			case 3:
+				{
+					i, err := strconv.Atoi(info)
+					if err != nil {
+						return
+					}
+					baths = append(baths, i)
+				}
+
+			case 4:
+				{
+					i, err := strconv.Atoi(info)
+					if err != nil {
+						return
+					}
+					prices = append(prices, i)
+				}
+			}
+
+		}
+	}
+
+	hdr := strings.Split(header, separator)
+	for _, h := range hdr {
+		fmt.Printf("%-15s", h)
+	}
+	fmt.Println()
+	fmt.Println(strings.Repeat("=", 80))
+
+	for i := 0; i < 4; i++ {
+		fmt.Printf("%-15s", location[i])
+		fmt.Printf("%-15d", size[i])
+		fmt.Printf("%-15d", beds[i])
+		fmt.Printf("%-15d", baths[i])
+		fmt.Printf("%-15d", prices[i])
+		fmt.Println()
+	} 
 }
