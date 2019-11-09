833d22e90295f192c5ad3620a4d33ad790498308
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2019-11-09 09:00:24 +0000

    refactor

--- slices/cmd/exercises.go
+++ slices/cmd/exercises.go
@@ -26,7 +26,8 @@ func main() {
 	// e2Append()
 	// e3Append()
 	// e3Sort()
-	e4HousePrices()
+	// e4HousePrices()
+	e5Slice()
 }
 
 // ---------------------------------------------------------
@@ -442,4 +443,89 @@ Istanbul,500,10,5,1000000`
 	fmt.Printf("%-15.2f", float64(bathsTotal)/float64(len(baths)))
 	fmt.Printf("%-15.2f", float64(pricesTotal)/float64(len(prices)))
 	fmt.Println()
+}
+
+// ---------------------------------------------------------
+// EXERCISE: Slice the numbers
+//
+//   We've a string that contains even and odd numbers.
+//
+//   1. Convert the string to an []int
+//
+//   2. Print the slice
+//
+//   3. Slice it for the even numbers and print it (assign it to a new slice variable)
+//
+//   4. Slice it for the odd numbers and print it (assign it to a new slice variable)
+//
+//   5. Slice it for the two numbers at the middle
+//
+//   6. Slice it for the first two numbers
+//
+//   7. Slice it for the last two numbers (use the len function)
+//
+//   8. Slice the evens slice for the last one number
+//
+//   9. Slice the odds slice for the last two numbers
+//
+//
+// EXPECTED OUTPUT
+//   go run main.go
+//
+//     nums        : [2 4 6 1 3 5]
+//     evens       : [2 4 6]
+//     odds        : [1 3 5]
+//     middle      : [6 1]
+//     first 2     : [2 4]
+//     last 2      : [3 5]
+//     evens last 1: [6]
+//     odds last 2 : [3 5]
+//
+//
+// NOTE
+//
+//  You can also use my prettyslice package for printing the slices.
+//
+//
+// HINT
+//
+//  Find a function in the strings package for splitting the string into []string
+//
+// ---------------------------------------------------------
+
+func e5Slice() {
+	// uncomment the declaration below
+	data := "2 4 6 1 3 5"
+	numStr := strings.Fields(data)
+
+	var num, odd, even []int
+	for _, v := range numStr {
+		i, err := strconv.Atoi(v)
+		if err != nil {
+			continue
+		}
+		num = append(num, i)
+		if i%2 == 0 {
+			even = append(even, i)
+		} else {
+			odd = append(odd, i)
+		}
+	}
+
+	middle := num[2:4]
+	firstTwo := num[:2]
+	lastTwo := num[len(num)-2:]
+	evenlast1 := even[len(even)-1:]
+	oddlast2 := odd[len(odd)-2:]
+
+
+	fmt.Printf("nums         :%d \n", num)
+	fmt.Printf("even         :%d \n", even)
+	fmt.Printf("odd          :%d \n", odd)
+	fmt.Printf("middle       :%d \n", middle)
+	fmt.Printf("first 2      :%d \n", firstTwo)
+	fmt.Printf("last 2       :%d \n", lastTwo)
+	fmt.Printf("evens last 1 :%d \n", evenlast1)
+	fmt.Printf("odd last 2   :%d \n", oddlast2)
+	
 }
