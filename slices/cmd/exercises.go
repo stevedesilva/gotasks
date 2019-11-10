6639860b1ef60a8767a53a7530474a251fa5074c
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2019-11-10 08:30:51 +0000

    new version

--- slices/cmd/exercises.go
+++ slices/cmd/exercises.go
@@ -633,7 +633,7 @@ func e5Slice() {
 //
 // ---------------------------------------------------------
 
-func slicingByArguments() {
+func slicingByArgumentsOld() {
 
 	const (
 		msg = `["Normandy" "Verrikan" "Nexus" "Warsaw"]
@@ -684,6 +684,35 @@ Wrong positions
 	}
 
 	slice := ships[starting:stopping]
-
+	fmt.Println("Cap ",cap(ships))
 	fmt.Println(slice)
+}
+
+
+func slicingByArguments() {
+	ships := []string{"Normandy", "Verrikan", "Nexus", "Warsaw"}
+
+	fmt.Printf("%q\n\n", ships)
+
+	from, to := 0, len(ships)
+
+	switch poss := os.Args[1:]; len(poss) {
+	default:
+		fallthrough
+	case 0:
+		fmt.Println("Provide only the [starting] and [stopping] positions")
+		return
+	case 2:
+		to, _ = strconv.Atoi(poss[1])
+		fallthrough
+	case 1:
+		from, _ = strconv.Atoi(poss[0])
+	}
+
+	if l := len(ships); from < 0 || from > l || to > l || from > to {
+		fmt.Println("Wrong positions")
+		return
+	}
+
+	fmt.Println(ships[from:to])
 }
