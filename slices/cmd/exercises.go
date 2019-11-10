794e8bf96a7ddc7020051ac09dd1d4e9773a04e5
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2019-11-10 10:38:39 +0000

    solution

--- slices/cmd/exercises.go
+++ slices/cmd/exercises.go
@@ -28,7 +28,8 @@ func main() {
 	// e3Sort()
 	// e4HousePrices()
 	// e5Slice()
-	slicingByArguments()
+	// slicingByArguments()
+	sliceHousePrices()
 }
 
 // ---------------------------------------------------------
@@ -313,140 +314,6 @@ func e3Sort() {
 }
 
 // ---------------------------------------------------------
-// EXERCISE: Housing Prices
-//
-//  We have received housing prices. Your task is to load the data into
-//  appropriately typed slices then print them.
-//
-//  1. Check out the expected output
-//
-//
-//  2. Check out the code below
-//
-//     1. header   : stores the column headers
-//     2. data     : stores the real data related to each column
-//     3. separator: you will use it to separate the data
-//
-//
-//  3. Parse the data
-//
-//     1. Separate it into rows by using the newline character ("\n")
-//
-//     2. For each row, separate it by using the separator (",")
-//
-//
-//  4. Load the data into distinct slices
-//
-//     1. Load the locations into a []string
-//     2. Load the sizes into []int
-//     3. Load the beds into []int
-//     4. Load the baths into []int
-//     5. Load the prices into []int
-//
-//
-//  5. Print the header
-//
-//     1. Separate it by using the separator
-//
-//     2. Print each column 15 character wide ("%-15s")
-//
-//
-//  6. Print the rows from the slices that you've created, line by line
-//
-//
-// EXPECTED OUTPUT
-//
-//  Location       Size           Beds           Baths          Price
-//  ===========================================================================
-//  New York       100            2              1              100000
-//  New York       150            3              2              200000
-//  Paris          200            4              3              400000
-//  Istanbul       500            10             5              1000000
-//
-//
-// HINTS
-//
-//  + strings.Split function can separate a string into a []string
-//
-// ---------------------------------------------------------
-
-func e4HousePrices() {
-	const (
-		header = "Location,Size,Beds,Baths,Price"
-		data   = `New York,100,2,1,100000
-New York,150,3,2,200000
-Paris,200,4,3,400000
-Istanbul,500,10,5,1000000`
-
-		separator = ","
-	)
-
-	var (
-		location                                      []string
-		size, beds, baths, prices                     []int
-		sizeTotal, bedsTotal, bathsTotal, pricesTotal int
-	)
-	rows := strings.Split(data, "\n")
-	for _, row := range rows {
-		col := strings.Split(row, separator)
-		location = append(location, col[0])
-
-		s, err := strconv.Atoi(col[1])
-		if err != nil {
-			return
-		}
-		sizeTotal += s
-		size = append(size, s)
-
-		b, err := strconv.Atoi(col[2])
-		if err != nil {
-			return
-		}
-		bedsTotal += b
-		beds = append(beds, b)
-
-		ba, err := strconv.Atoi(col[3])
-		if err != nil {
-			return
-		}
-		bathsTotal += ba
-		baths = append(baths, ba)
-
-		p, err := strconv.Atoi(col[4])
-		if err != nil {
-			return
-		}
-		pricesTotal += p
-		prices = append(prices, p)
-
-	}
-
-	hdr := strings.Split(header, separator)
-	for _, h := range hdr {
-		fmt.Printf("%-15s", h)
-	}
-	fmt.Println()
-	fmt.Println(strings.Repeat("=", 80))
-
-	for i := range rows {
-		fmt.Printf("%-15s", location[i])
-		fmt.Printf("%-15d", size[i])
-		fmt.Printf("%-15d", beds[i])
-		fmt.Printf("%-15d", baths[i])
-		fmt.Printf("%-15d", prices[i])
-		fmt.Println()
-	}
-
-	fmt.Println(strings.Repeat("=", 80))
-	fmt.Printf("%-15s", "")
-	fmt.Printf("%-15.2f", float64(sizeTotal)/float64(len(size)))
-	fmt.Printf("%-15.2f", float64(bedsTotal)/float64(len(beds)))
-	fmt.Printf("%-15.2f", float64(bathsTotal)/float64(len(baths)))
-	fmt.Printf("%-15.2f", float64(pricesTotal)/float64(len(prices)))
-	fmt.Println()
-}
-
-// ---------------------------------------------------------
 // EXERCISE: Slice the numbers
 //
 //   We've a string that contains even and odd numbers.
@@ -684,10 +551,9 @@ Wrong positions
 	}
 
 	slice := ships[starting:stopping]
-	fmt.Println("Cap ",cap(ships))
+	fmt.Println("Cap ", cap(ships))
 	fmt.Println(slice)
 }
-
 
 func slicingByArguments() {
 	ships := []string{"Normandy", "Verrikan", "Nexus", "Warsaw"}
@@ -715,4 +581,306 @@ func slicingByArguments() {
 	}
 
 	fmt.Println(ships[from:to])
+}
+
+// ---------------------------------------------------------
+// EXERCISE: Housing Prices
+//
+//  We have received housing prices. Your task is to load the data into
+//  appropriately typed slices then print them.
+//
+//  1. Check out the expected output
+//
+//
+//  2. Check out the code below
+//
+//     1. header   : stores the column headers
+//     2. data     : stores the real data related to each column
+//     3. separator: you will use it to separate the data
+//
+//
+//  3. Parse the data
+//
+//     1. Separate it into rows by using the newline character ("\n")
+//
+//     2. For each row, separate it by using the separator (",")
+//
+//
+//  4. Load the data into distinct slices
+//
+//     1. Load the locations into a []string
+//     2. Load the sizes into []int
+//     3. Load the beds into []int
+//     4. Load the baths into []int
+//     5. Load the prices into []int
+//
+//
+//  5. Print the header
+//
+//     1. Separate it by using the separator
+//
+//     2. Print each column 15 character wide ("%-15s")
+//
+//
+//  6. Print the rows from the slices that you've created, line by line
+//
+//
+// EXPECTED OUTPUT
+//
+//  Location       Size           Beds           Baths          Price
+//  ===========================================================================
+//  New York       100            2              1              100000
+//  New York       150            3              2              200000
+//  Paris          200            4              3              400000
+//  Istanbul       500            10             5              1000000
+//
+//
+// HINTS
+//
+//  + strings.Split function can separate a string into a []string
+//
+// ---------------------------------------------------------
+
+func e4HousePrices() {
+	const (
+		header = "Location,Size,Beds,Baths,Price"
+		data   = `New York,100,2,1,100000
+New York,150,3,2,200000
+Paris,200,4,3,400000
+Istanbul,500,10,5,1000000`
+
+		separator = ","
+	)
+
+	var (
+		location                                      []string
+		size, beds, baths, prices                     []int
+		sizeTotal, bedsTotal, bathsTotal, pricesTotal int
+	)
+	rows := strings.Split(data, "\n")
+	for _, row := range rows {
+		col := strings.Split(row, separator)
+		location = append(location, col[0])
+
+		s, err := strconv.Atoi(col[1])
+		if err != nil {
+			return
+		}
+		sizeTotal += s
+		size = append(size, s)
+
+		b, err := strconv.Atoi(col[2])
+		if err != nil {
+			return
+		}
+		bedsTotal += b
+		beds = append(beds, b)
+
+		ba, err := strconv.Atoi(col[3])
+		if err != nil {
+			return
+		}
+		bathsTotal += ba
+		baths = append(baths, ba)
+
+		p, err := strconv.Atoi(col[4])
+		if err != nil {
+			return
+		}
+		pricesTotal += p
+		prices = append(prices, p)
+
+	}
+
+	hdr := strings.Split(header, separator)
+	for _, h := range hdr {
+		fmt.Printf("%-15s", h)
+	}
+	fmt.Println()
+	fmt.Println(strings.Repeat("=", 80))
+
+	for i := range rows {
+		fmt.Printf("%-15s", location[i])
+		fmt.Printf("%-15d", size[i])
+		fmt.Printf("%-15d", beds[i])
+		fmt.Printf("%-15d", baths[i])
+		fmt.Printf("%-15d", prices[i])
+		fmt.Println()
+	}
+
+	fmt.Println(strings.Repeat("=", 80))
+	fmt.Printf("%-15s", "")
+	fmt.Printf("%-15.2f", float64(sizeTotal)/float64(len(size)))
+	fmt.Printf("%-15.2f", float64(bedsTotal)/float64(len(beds)))
+	fmt.Printf("%-15.2f", float64(bathsTotal)/float64(len(baths)))
+	fmt.Printf("%-15.2f", float64(pricesTotal)/float64(len(prices)))
+	fmt.Println()
+}
+
+// ---------------------------------------------------------
+// EXERCISE: Slicing the Housing Prices
+//
+//  We have received housing prices. Your task is to print only the requested
+//  columns of data (see the expected output).
+//
+//  1. Separate the data by the newline ("\n") -> rows
+//
+//  2. Separate each row of the data by the separator (",") -> columns
+//
+//  3. Find the from and to positions inside the columns depending
+//     on the command-line arguments.
+//
+//  4. Print only the requested column headers and their data
+//
+//
+// RESTRICTIONS
+//
+//  + You should use slicing when printing the columns.
+//
+//
+// EXPECTED OUTPUT
+//
+//  go run main.go
+//    Location       Size           Beds           Baths          Price
+//
+//    New York       100            2              1              100000
+//    New York       150            3              2              200000
+//    Paris          200            4              3              400000
+//    Istanbul       500            10             5              1000000
+//
+//
+//  go run main.go Location
+//    Location       Size           Beds           Baths          Price
+//
+//    New York       100            2              1              100000
+//    New York       150            3              2              200000
+//    Paris          200            4              3              400000
+//    Istanbul       500            10             5              1000000
+//
+//
+//  NOTE : Finds the position of the Size column and slices the columns
+//         appropriately.
+//
+//  go run main.go Size
+//    Size           Beds           Baths          Price
+//
+//    100            2              1              100000
+//    150            3              2              200000
+//    200            4              3              400000
+//    500            10             5              1000000
+//
+//
+//  NOTE : Finds the positions of the Size and Baths columns and
+//         slices the columns appropriately.
+//
+//  go run main.go Size Baths
+//    Size           Beds           Baths
+//
+//    100            2              1
+//    150            3              2
+//    200            4              3
+//    500            10             5
+//
+//
+//  go run main.go Beds Price
+//    Beds           Baths          Price
+//
+//    2              1              100000
+//    3              2              200000
+//    4              3              400000
+//    10             5              1000000
+//
+//
+//  Note : It works even if the given column name does not exist.
+//
+//  go run main.go Beds NotExist
+//    Beds           Baths          Price
+//
+//    2              1              100000
+//    3              2              200000
+//    4              3              400000
+//    10             5              1000000
+//
+//
+//  go run main.go NotExist NotExist
+//    Location       Size           Beds           Baths          Price
+//
+//    New York       100            2              1              100000
+//    New York       150            3              2              200000
+//    Paris          200            4              3              400000
+//    Istanbul       500            10             5              1000000
+//
+//
+// Note : It works even if the Price's index > Size's index
+//
+//        In that case, it resets the invalid starting position to 0.
+//
+//        But it still uses the Size column.
+//
+//  go run main.go Price Size
+//    Location       Size
+//
+//    New York       100
+//    New York       150
+//    Paris          200
+//    Istanbul       500
+//
+//
+// HINTS
+//
+//  + strings.Split function can separate a string into a []string
+//
+// ---------------------------------------------------------
+
+func sliceHousePrices() {
+	const (
+		data = `Location,Size,Beds,Baths,Price
+New York,100,2,1,100000
+New York,150,3,2,200000
+Paris,200,4,3,400000
+Istanbul,500,10,5,1000000`
+
+		separator = ","
+	)
+
+	// parse the data
+	rows := strings.Split(data, "\n")
+	cols := strings.Split(rows[0], separator)
+
+	// default case: slice for all the columns
+	from, to := 0, len(cols)
+
+	// find the from and to positions depending on the command-line arguments
+	args := os.Args[1:]
+	for i, v := range cols {
+		l := len(args)
+
+		if l >= 1 && v == args[0] {
+			from = i
+		}
+
+		if l == 2 && v == args[1] {
+			to = i + 1 // +1 because the stopping index is a position
+		}
+	}
+
+	// from cannot be greater than to: reset invalid arg to 0
+	if from > to {
+		from = 0
+	}
+
+	for i, row := range rows {
+		cols := strings.Split(row, separator)
+
+		// print only the requested columns
+		for _, h := range cols[from:to] {
+			fmt.Printf("%-15s", h)
+		}
+		fmt.Println()
+
+		// print extra new line for the header
+		if i == 0 {
+			fmt.Println()
+		}
+	}
 }
