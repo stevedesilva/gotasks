84a8434bec724d69b7001e6f08c9234fcca5b7e7
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2019-11-29 14:03:21 +0000

    memory leak exe

--- slices/cmd/exeadvanced/e2/main.go
+++ slices/cmd/exeadvanced/e2/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/stevedesilva/learngo/slices/api"
+	"github.com/stevedesilva/learngo/slices/cmd/exeadvanced/e2/api"
 )
 
 // ---------------------------------------------------------
