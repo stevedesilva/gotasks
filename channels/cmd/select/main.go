1d43d535046453bcf7b025b3924fb838484fe997
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2019-12-11 11:35:11 +0000

    new comma ok idiom

--- channels/cmd/select/main.go
+++ channels/cmd/select/main.go
@@ -3,33 +3,23 @@ package main
 import "fmt"
 
 func main() {
+	sendAndReceive()
+	commaOkIdiom()
+}
+
+func sendAndReceive() {
 	eve := make(chan int)
 	odd := make(chan int)
-	quit := make(chan int)
+	quit := make(chan bool)
 
 	go send(eve, odd, quit)
 
 	receive(eve, odd, quit)
 
 	fmt.Println("The end!!!!")
-
 }
 
-func receive(eve, odd, quit <-chan int) {
-	for {
-		select {
-		case v := <-eve:
-			fmt.Println("from the eve channel:", v)
-		case v := <-odd:
-			fmt.Println("from the odd channel:", v)
-		case v := <-quit:
-			fmt.Println("from the quit channel:", v)
-			return
-		}
-	}
-}
-
-func send(eve, odd, quit chan<- int) {
+func send(eve, odd chan<- int, quit chan<- bool) {
 	for i := 0; i < 100; i++ {
 		if i%2 == 0 {
 			eve <- i
@@ -37,7 +27,40 @@ func send(eve, odd, quit chan<- int) {
 			odd <- i
 		}
 	}
-	close(eve)
-	close(odd)
-	quit <- 0
+	// close(eve)
+	// close(odd)
+	close(quit)
+}
+
+func receive(eve, odd <-chan int, quit <-chan bool) {
+	for {
+		select {
+		case v := <-eve:
+			fmt.Println("from the eve channel:", v)
+		case v := <-odd:
+			fmt.Println("from the odd channel:", v)
+		case i, ok := <-quit:
+			if !ok {
+				fmt.Println("from the quit channel:", i, ok)
+				return
+			}
+			fmt.Println("from the quit channel:", i, ok)
+		}
+	}
+}
+
+func commaOkIdiom() {
+	fmt.Println("Hello, playground")
+
+	in := make(chan int)
+	go func(in chan<- int) {
+		in <- 42
+		close(in)
+	}(in)
+
+	out, ok := <-in
+	fmt.Println("Out: ", out, ok)
+	out, ok = <-in
+	fmt.Println("Out 2: ", out, ok)
+
 }
